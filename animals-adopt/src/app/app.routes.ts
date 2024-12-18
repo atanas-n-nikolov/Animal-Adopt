2d3299dab0bcc7f97f4147632775644ae62a6f20
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-12-18 18:35:01 +0200

    Add login and register component
    html and styles

--- animals-adopt/src/app/app.routes.ts
+++ animals-adopt/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './user/register/register.component';
+import { LoginComponent } from './user/login/login.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'users/register', component: RegisterComponent},
+  {path: 'users/login', component: LoginComponent}
 ];
