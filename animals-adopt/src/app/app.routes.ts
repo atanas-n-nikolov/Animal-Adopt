02af0b90aa7c0e746e558723f10521ff5400055a
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-12-20 16:41:03 +0200

    Add adopt component
    and other components

--- animals-adopt/src/app/app.routes.ts
+++ animals-adopt/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './user/register/register.component';
 import { LoginComponent } from './user/login/login.component';
+import { AdoptComponent } from './adopt/adopt.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'users/register', component: RegisterComponent},
-  {path: 'users/login', component: LoginComponent}
+  {path: 'users/login', component: LoginComponent},
+  {path: 'animals/adopt', component: AdoptComponent}
 ];
---
+++ animals-adopt/src/app/user/add-animal/add-animal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-animal',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-animal.component.html',
+  styleUrl: './add-animal.component.css'
+})
+export class AddAnimalComponent {
+
+}
